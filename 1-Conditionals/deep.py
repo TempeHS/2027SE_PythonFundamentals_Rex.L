e44cfd8e5c319f58caa87f39aad2a3639051274c
Rex0Liang <rex.liang@education.nsw.gov.au>
2026-02-07 21:17:56 +1100

    Upload completed deep.py

---
+++ 1-Conditionals/deep.py
@@ -0,0 +1,6 @@
+userAnswer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower()
+
+if userAnswer == "42" or userAnswer == "forty-two" or userAnswer == "forty two":
+      print("Yes")
+else:
+      print("No")
