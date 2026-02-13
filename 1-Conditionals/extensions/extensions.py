1e329ff76423e7f93517db3935a50702b490ab91
Rex L <159222539+Rex0Liang@users.noreply.github.com>
2026-02-13 11:10:50 +1100

    Upload completed extensions.py

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,17 +1,16 @@
 userFile = input("Enter a file name: ").lower()
 
-match userFile:
-      case userFile.endswith("gif"):
-            print("image/gif")
-      case userFile.endswith("jpg") | userFile.endswith("jpeg"):
-            print("image/jpeg")
-      case userFile.endswith("png"):
-            print("image/png")
-      case userFile.endswith("pdf"):
-            print("application/pdf")
-      case userFile.endswith("txt"):
-            print("text/plain")
-      case userFile.endswith("zip"):
-            print("application/zip")
-      case _:
-            print("application/octet-stream")
+if userFile.endswith("gif"):
+      print("image/gif")
+elif userFile.endswith("jpg") or userFile.endswith("jpeg"):
+      print("image/jpeg")
+elif userFile.endswith("png"):
+      print("image/png")
+elif userFile.endswith("pdf"):
+      print("application/pdf")
+elif userFile.endswith("txt"):
+      print("text/plain")
+elif userFile.endswith("zip"):
+      print("application/zip")
+else:
+      print("application/octet-stream")
