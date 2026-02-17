6fcc4b8a25da2b2cc8004507ad8006a12b68c40a
Rex L <159222539+Rex0Liang@users.noreply.github.com>
2026-02-17 14:14:30 +1100

    WIP: Problem 2.2

--- 2-Loops/coke/coke.py
+++ 2-Loops/coke/coke.py
@@ -1,20 +1,26 @@
+coke_cost = 50
+amount_due = 0
+user_input = ""
+
 def main():
-      user_input = int(input("Insert a coin: "))
-      coke_cost = 50
-      amount_payed = 0
+      getUserInput()
       
       while amount_payed < coke_cost:
-            amount_payed += user_input
-            
             if amount_payed >= coke_cost:
                   break
-            elif user_input != 25 or 10 or 5:
-                  print("Not accepted, try again")
-                  user_input = int(input("Insert a coin: "))
+            elif user_input != 25 and user_input != 10 and user_input != 5:
+                  print("Coin not supported, please try again: ")
+                  getUserInput()
             else:
-                  amount_payed += user_input
-            
-            
-            
-            
+                  amount_payed = amount_payed + user_input
+                  amount_due = coke_cost - amount_payed
+                  print(amount_due)
+      
+      
+def getUserInput():
+      user_input = input("Insert a coin: ")
+      amount_payed = amount_payed + user_input
+      return amount_payed
+      
+      
 main()
