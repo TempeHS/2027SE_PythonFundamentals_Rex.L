a4928e9f49178a40706e0959ebfa56fdf2eb565a
Rex0Liang <rex.liang@education.nsw.gov.au>
2026-02-24 09:04:32 +1100

    Upload completed fuel.py

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -5,13 +5,20 @@ def main():
       
       while True:
             user_input = input("How much fuel is left? ")
+            
             try:
                   parsed_fuel = user_input.split("/")
-                  percent_fuel = (float(parsed_fuel[0]) / float(parsed_fuel[1])) * 100
+                  numer = float(parsed_fuel[0])
+                  denom = float(parsed_fuel[1])
+                  percent_fuel = numer / denom * 100
+                  
+                  if numer > denom:
+                        raise ValueError("Invalid input, try again.")
                   break
             except (ValueError, ZeroDivisionError):
                   print("Invalid input, try again.")
-
+            
+                  
       if percent_fuel <= 1:
             print(empty)
       elif percent_fuel >= 99:
