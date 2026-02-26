57cd434e7ab1a1d4783638dfba72aa6571d84fa4
Rex0Liang <rex.liang@education.nsw.gov.au>
2026-02-26 14:47:08 +1100

    Upload completed outdated.py

--- 3-Exceptions/outdated/outdated.py
+++ 3-Exceptions/outdated/outdated.py
@@ -18,15 +18,25 @@ def main():
             try:
                   user_input = input("Enter a date: ")
                   month, day, year = user_input.split("/")
+                  if int(month) > 12 or int(month) < 1:
+                        continue
                   print(f"{int(year)}-{int(month):02}-{int(day):02}")
                   break
             except ValueError:
                   try:
                         day_month, year = user_input.split(",")
-                        day, _, month = day_month.partition(" ")
-
+                        month, _, day = day_month.partition(" ")
+                        month = month.title()
                         try:
-                              month in months
+                              if month in months:
+                                    month_num = months.index(month) + 1
+                                    
+                                    print(f"{int(year)}-{int(month_num):02}-{int(day):02}")
+                                    break
+                              else:
+                                    continue
+                        except ValueError:
+                              continue
                         
                   except ValueError:
                         print("Invalid input, try again.")
