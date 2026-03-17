cd60478b2392b2a6e631edceffb708c19177a613
Rex0Liang <rex.liang@education.nsw.gov.au>
2026-03-17 12:08:58 +1100

    Upload completed pizza.py

--- 6-FileIO/pizza/pizza.py
+++ 6-FileIO/pizza/pizza.py
@@ -8,15 +8,21 @@ def main():
             
             if len(sys.argv) > 2:
                   raise IndexError
+            elif user_input.endswith(".csv"):
+                  pass
+            else:
+                  raise ValueError
       except IndexError:
             sys.exit("Too few/many command-line arguments")
+      except ValueError:
+            sys.exit("Not a CSV file")
       
       
       try:
             with open(user_input, "r") as csvfile:
                   print(csvfile)
                   menu = csv.DictReader(csvfile)
-                  print(t(menu, headers="firstrow", tablefmt="grid"))
+                  print(t(menu, headers="keys", tablefmt="grid"))
       except FileNotFoundError:
             sys.exit("File not found")
             
