a94ad43dcb8dda24695ab24c0e054f8248dc0c2a
Rex0Liang <rex.liang@education.nsw.gov.au>
2026-03-25 18:19:29 +1100

    Upload completed scourgify.py

--- 6-FileIO/scourgify/scourgify.py
+++ 6-FileIO/scourgify/scourgify.py
@@ -10,21 +10,36 @@ def main():
                   raise IndexError
             elif old_user_file.endswith(".csv") and new_user_file.endswith(".csv"):
                   pass
-                  print(len(sys.argv))
             else:
                   raise ValueError
       except IndexError:
             sys.exit("Too few/many command-line arguments")
       except ValueError:
             sys.exit("Invalid file type")
+
             
       read_file(old_user_file, new_user_file)
 
-def read_file(old, new):
-      with open(old, "w", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                  print(row)            
 
+def read_file(old, new):
+      try:
+            with open(old, "r", newline="") as csvfile:
+                  reader = csv.DictReader(csvfile)
                   
+                  with open(new, "w", newline="") as final_file:
+                        layout = ["first_name", "last_name", "house"]
+                        writer = csv.DictWriter(final_file, fieldnames=layout)  
+                        writer.writeheader()
+                  
+                        for row in reader:
+                              last_name, first_name = row["name"].split(",")
+                              writer.writerow(
+                                    {"first_name": first_name.strip(), 
+                                    "last_name": last_name.strip(), 
+                                    "house": row["house"].strip()
+                                    })
+      except FileNotFoundError:
+            sys.exit(f"Could not read {sys.argv[1]}")
+            
+      
 main()
